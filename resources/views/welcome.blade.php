17c6dc65796fe612f92fb8213909e664da19cf27
DzakiNur <dzakinurmuhammadaflahsuherman@smkwikrama.sch.id>
2023-01-26 15:23:13 +0700

    commit gue

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rental Buku</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+</head>
+<body>
+    <!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+  <div class="container">
+  <a class="navbar-brand" href="#">
+      <img src="assets/img/Rb(2).png" alt="Logo" width="36" height="30" class="d-inline-block align-text-top">
+      RentBook
+    </a> 
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Category
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">horror</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Comedy</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link btn btn-dark" href="#">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    <!-- akhir dari navbar -->
+    <!-- carousel -->
+    <div id="carouselExampleInterval" class="carousel slide" style="max-height: 300px; max: width 100%;" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="3000">
+      <img src="https://img.freepik.com/premium-vector/gradient-flash-sale-background-template-banner_554907-292.jpg?w=1060" class="d-block w-100" alt="..." style="max-height: 300px; max-width: 10000px">
+    </div>
+    <div class="carousel-item" data-bs-interval="3000">
+      <img src="https://img.freepik.com/premium-psd/banner-offer-hour-brazil-render-3d-template-design-portuguese_363450-1430.jpg?w=1060" class="d-block w-100" alt="..." style="max-height: 300px; max-width: 10000px">
+    </div>
+    <div class="carousel-item" data-bs-interval="3000">
+      <img src="https://img.freepik.com/free-vector/mega-sale-social-media-posts-with-discount_52683-45314.jpg?w=996&t=st=1674714140~exp=1674714740~hmac=39259202bced7bacbfd24a5d19d90ef455691380bb57be87a5bab13f78d09927" class="d-block w-100" alt="..." style="max-height: 300px; max-width: 10000px">
+    </div>
+    <div class="carousel-item" data-bs-interval="3000">
+      <img src="https://indopolitika.com/wp-content/uploads/2019/07/PDIP-3.jpg" class="d-block w-100" alt="..." style="max-height: 300px; max-width: 10000px">
+    </div>
+    <div class="carousel-item" data-bs-interval="3000">
+      <img src="assets/img/fikri.jpg" class="d-block w-100" alt="..." style="max-height: 300px; max-width: 10000px">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    <!-- akhir dari carousel -->
+
+    <!-- autfurecqr9 -->
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>PDI Perjuangan</h2>
+                <p>Bersama PDI membangun Negeri</p>
+                <a href="#" class="btn btn-outline-danger">Ayo bergabung bersama kami</a>
+            </div>
+            <div class="col-lg-8">
+                <p>Partai Demokrasi Indonesia Perjuangan adalah sebuah partai politik Indonesia, dan partai dari Presiden Indonesia saat ini, Joko Widodo. PDI-P saat ini dipimpin oleh Megawati Soekarnoputri, Presiden Kelima Indonesia dan merupakan putri dari Presiden Pertama Indonesia Soekarno.</p>
+            </div>
+        </div>
+    </div>
+    <!--  -->
+    <!-- Card popular books -->
+    <section style="background-color: #820000">
+        <div class="container mt-5">
+            <div class="row text-center text-white">
+                <h2 class="mt-4 mb-4">Popular Books</h2><hr class="solid">
+            </div>
+            <div class="row mt-4 ms-5">
+                <div class="col-lg-4 mb-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1266068843i/7725825.jpg" class="card-img-top class="style="max-height: 300px" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-danger w-100">Read</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://inc.mizanstore.com/aassets/img/com_cart/produk/covBK001324.jpg" class="card-img-top class="style="max-height: 300px" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-danger w-100">Read</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://www.gramedia.com/blog/content/images/2022/06/Cantik-Itu-Luka.jpg" class="card-img-top class="style="max-height: 300px" >
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-danger w-100">Read</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<!-- akhir card -->
+
+<!-- Contact -->
+    <div class="container">
+        <div class="row text-center">
+            <h2 class="mt-4 mb-4">Message</h2>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-7">
+                <form>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="messege" class="form-label">Message</label>
+                        <textarea name="messege"class="form-control" id="messege"> </textarea>
+                    </div>
+                    <button type="submit" class="btn btn-danger">Send</button>
+                </form>
+            </div>
+        </div>
+    </div>
+<!-- Akhir contact -->
+<!-- Footer -->
+<footer class="mt-5 bg-danger text-center" style="height: 20px;">
+    <p> Created By <a href="https://www.instagram.com/k.abduul3/" target="_blank" style="text-decoration: none; color: white"> Kiki Ganteng Icikiwir</a></p>
+</footer>
+<!-- Akhir Footer -->
+</body> 
+</html>
